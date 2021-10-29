16e1f7f39f0bcda10916ab1c5dcae71c51ae5229
AlexanderPalatkin <palatkin.alexander@gmail.com>
2021-10-29 20:03:04 +0300

    Convert to Kotlin classes

--- app/src/main/java/com/example/notebook/NoteEntity.kt
+++ app/src/main/java/com/example/notebook/NoteEntity.kt
@@ -1,83 +1,71 @@
-package com.example.notebook;
+package com.example.notebook
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import android.os.Parcelable
+import android.os.Parcel
+import android.os.Parcelable.Creator
+import java.util.*
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.UUID;
+class NoteEntity : Parcelable {
+    val id: String?
+    var title: String?
+        private set
+    var description: String?
+        private set
+    var date: Date? = null
+        private set
 
-public class NoteEntity implements Parcelable {
-    public static final Creator<NoteEntity> CREATOR = new Creator<NoteEntity>() {
-        @Override
-        public NoteEntity createFromParcel(Parcel in) {
-            return new NoteEntity(in);
+    internal constructor(
+        id: String?,
+        title: String?,
+        description: String?,
+        date: Date?
+    ) {
+        this.id = id
+        this.title = title
+        this.description = description
+        this.date = date
+    }
+
+    protected constructor(`in`: Parcel) {
+        id = `in`.readString()
+        title = `in`.readString()
+        description = `in`.readString()
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(id)
+        dest.writeString(title)
+        dest.writeString(description)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    fun update(newNote: NoteEntity) {
+        description = newNote.description
+        title = newNote.title
+    }
+
+    companion object {
+        @JvmField
+        val CREATOR: Creator<NoteEntity?> = object : Creator<NoteEntity?> {
+            override fun createFromParcel(`in`: Parcel): NoteEntity? {
+                return NoteEntity(`in`)
+            }
+
+            override fun newArray(size: Int): Array<NoteEntity?> {
+                return arrayOfNulls(size)
+            }
         }
 
-        @Override
-        public NoteEntity[] newArray(int size) {
-            return new NoteEntity[size];
+        @JvmStatic
+        fun generateNewId(): String {
+            return UUID.randomUUID().toString()
         }
-    };
-    private final String id;
-    private String title;
-    private String description;
-    private Date date;
 
-    NoteEntity(String id,
-               String title,
-               String description,
-               Date date) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.date = date;
-    }
-
-    protected NoteEntity(Parcel in) {
-        id = in.readString();
-        title = in.readString();
-        description = in.readString();
-    }
-
-    public static String generateNewId() {
-        return UUID.randomUUID().toString();
-    }
-
-    public static Date getCurrentDate() {
-        return Calendar.getInstance().getTime();
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(id);
-        dest.writeString(title);
-        dest.writeString(description);
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void update(NoteEntity newNote) {
-        description = newNote.description;
-        title = newNote.title;
+        @JvmStatic
+        val currentDate: Date
+            get() = Calendar.getInstance().time
     }
 }
--- app/src/main/java/com/example/notebook/NoteListFragment.kt
+++ app/src/main/java/com/example/notebook/NoteListFragment.kt
@@ -1,260 +1,252 @@
-package com.example.notebook;
+package com.example.notebook
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.app.AlertDialog
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import android.os.Bundle
+import com.google.firebase.FirebaseApp
+import android.content.DialogInterface
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.DividerItemDecoration
+import android.widget.LinearLayout
+import android.util.Log
+import android.view.*
+import android.widget.Button
+import androidx.core.content.res.ResourcesCompat
+import androidx.fragment.app.Fragment
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.*
+import java.lang.RuntimeException
+import java.util.*
 
-import androidx.annotation.NonNull;
-import androidx.core.content.res.ResourcesCompat;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-
-public class NoteListFragment extends Fragment {
-
-    private static final String TAG = "NoteSourceFirebaseImpl";
-    private static int positionToList;
-    private final String ACTION_DEL_NOTE = "ACTION_DEL_NOTE";
-    private final ArrayList<NoteEntity> noteList = new ArrayList<>();
-    private boolean SET_UPDATE_NOTE = false;
-    private Button createButton;
-    private RecyclerView recyclerView;
-    private NotesAdapter adapter;
-    private CollectionReference noteCollection;
-
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        if (!(context instanceof Controller)) {
-            throw new RuntimeException("Activity must implement Controller");
+class NoteListFragment : Fragment() {
+    private val ACTION_DEL_NOTE = "ACTION_DEL_NOTE"
+    private val noteList = ArrayList<NoteEntity>()
+    private var SET_UPDATE_NOTE = false
+    private var createButton: Button? = null
+    private var recyclerView: RecyclerView? = null
+    private var adapter: NotesAdapter? = null
+    private var noteCollection: CollectionReference? = null
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context !is Controller) {
+            throw RuntimeException("Activity must implement Controller")
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_note_list, container, false);
-        createButton = view.findViewById(R.id.create_note_button);
-        recyclerView = view.findViewById(R.id.recycler_view);
-
-        recyclerView.setHasFixedSize(true);
-        setHasOptionsMenu(true);
-
-        FirebaseApp.initializeApp(requireContext());
-        FirebaseFirestore myDB = FirebaseFirestore.getInstance();
-        noteCollection = myDB.collection("notes");
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_note_list, container, false)
+        createButton = view.findViewById(R.id.create_note_button)
+        recyclerView = view.findViewById(R.id.recycler_view)
+        recyclerView?.setHasFixedSize(true)
+        setHasOptionsMenu(true)
+        FirebaseApp.initializeApp(requireContext())
+        val myDB = FirebaseFirestore.getInstance()
+        noteCollection = myDB.collection("notes")
         if (!SET_UPDATE_NOTE) {
-            initListBD(noteList);
-            SET_UPDATE_NOTE = false;
+            initListBD(noteList)
+            SET_UPDATE_NOTE = false
         }
-        return view;
+        return view
     }
 
-    @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.note_list_menu, menu);
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.note_list_menu, menu)
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.note_list_clear) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        return if (id == R.id.note_list_clear) {
+            val builder = AlertDialog.Builder(requireContext())
             builder.setTitle(R.string.warning)
-                    .setMessage(R.string.clear_note_list)
-                    .setCancelable(false)
-                    .setPositiveButton(R.string.yes_clear_list,
-                            (dialog, which) -> {
-                                for (NoteEntity note : noteList) {
-                                    deleteFromBD(note);
-                                }
-                                noteList.clear();
-                                renderList(noteList);
-                            })
-                    .setNegativeButton(R.string.no_clear_list, null);
-            AlertDialog alertClearList = builder.create();
-            alertClearList.show();
-            return true;
-        } else {
-            return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        adapter = new NotesAdapter();
-        adapter.setOnItemClickListener((position, action) -> {
-            if (action == adapter.ACTION_CHANGE) {
-                getController().editNote(noteList.get(position));
-                adapter.notifyItemChanged(position);
-            }
-            if (action == adapter.ACTION_DELETE) {
-                noteList.remove(position);
-                adapter.notifyItemRemoved(position);
-            }
-        });
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(adapter);
-        DividerItemDecoration itemDecoration = new DividerItemDecoration(requireActivity(), LinearLayout.VERTICAL);
-        itemDecoration.setDrawable(Objects.requireNonNull(ResourcesCompat.getDrawable(getResources(), R.drawable.separator, null)));
-        recyclerView.addItemDecoration(itemDecoration);
-        renderList(noteList);
-        createButton.setOnClickListener(v -> {
-            getController().createNewNote();
-        });
-    }
-
-    public void addNote(NoteEntity newNote) {
-        NoteEntity sameNote = findNoteWithId(newNote.getId());
-        if (sameNote != null) {
-            updateNoteFromBD(newNote);
-            SET_UPDATE_NOTE = true;
-            sameNote.update(newNote);
-        } else {
-            addNoteToBD(newNote);
-            noteList.add(newNote);
-        }
-        renderList(noteList);
-    }
-
-    public void deleteNote(NoteEntity delNote) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
-        builder.setTitle(R.string.warning)
-                .setMessage(R.string.delete_note)
+                .setMessage(R.string.clear_note_list)
                 .setCancelable(false)
-                .setPositiveButton(R.string.yes_delete_note,
-                        (dialog, which) -> {
-                            NoteEntity sameNote = findNoteWithId(delNote.getId());
-                            if (sameNote != null) {
-                                noteList.remove(sameNote);
-                            }
-                            deleteFromBD(sameNote);
-                            renderList(noteList, ACTION_DEL_NOTE);
-                        })
-                .setNegativeButton(R.string.no_clear_list, null);
-        AlertDialog alertClearList = builder.create();
-        alertClearList.show();
+                .setPositiveButton(
+                    R.string.yes_clear_list
+                ) { dialog: DialogInterface?, which: Int ->
+                    for (note in noteList) {
+                        deleteFromBD(note)
+                    }
+                    noteList.clear()
+                    renderList(noteList)
+                }
+                .setNegativeButton(R.string.no_clear_list, null)
+            val alertClearList = builder.create()
+            alertClearList.show()
+            true
+        } else {
+            super.onOptionsItemSelected(item)
+        }
     }
 
-    private NoteEntity findNoteWithId(String id) {
-        for (NoteEntity note : noteList) {
-            if (note.getId().equals(id)) {
-                return note;
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        adapter = NotesAdapter()
+        adapter!!.setOnItemClickListener { position: Int, action: Int ->
+            if (action == adapter!!.ACTION_CHANGE) {
+                controller!!.editNote(noteList[position])
+                adapter!!.notifyItemChanged(position)
+            }
+            if (action == adapter!!.ACTION_DELETE) {
+                noteList.removeAt(position)
+                adapter!!.notifyItemRemoved(position)
             }
         }
-        return null;
+        recyclerView!!.layoutManager = LinearLayoutManager(context)
+        recyclerView!!.adapter = adapter
+        val itemDecoration = DividerItemDecoration(requireActivity(), LinearLayout.VERTICAL)
+        itemDecoration.setDrawable(
+            Objects.requireNonNull(
+                ResourcesCompat.getDrawable(
+                    resources, R.drawable.separator, null
+                )
+            )!!
+        )
+        recyclerView!!.addItemDecoration(itemDecoration)
+        renderList(noteList)
+        createButton!!.setOnClickListener { v: View? -> controller!!.createNewNote() }
     }
 
-    private void renderList(ArrayList<NoteEntity> noteList) {
-        adapter.setData(noteList);
-        adapter.notifyDataSetChanged();
+    fun addNote(newNote: NoteEntity) {
+        val sameNote = findNoteWithId(newNote.id.toString())
+        if (sameNote != null) {
+            updateNoteFromBD(newNote)
+            SET_UPDATE_NOTE = true
+            sameNote.update(newNote)
+        } else {
+            addNoteToBD(newNote)
+            noteList.add(newNote)
+        }
+        renderList(noteList)
     }
 
-    private void renderList(List<NoteEntity> notes, String action) {
-        adapter.setData(notes);
-        switch (action) {
-            case ACTION_DEL_NOTE:
-                adapter.notifyItemRemoved(positionToList);
+    fun deleteNote(delNote: NoteEntity) {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle(R.string.warning)
+            .setMessage(R.string.delete_note)
+            .setCancelable(false)
+            .setPositiveButton(
+                R.string.yes_delete_note
+            ) { dialog: DialogInterface?, which: Int ->
+                val sameNote = findNoteWithId(delNote.id.toString())
+                if (sameNote != null) {
+                    noteList.remove(sameNote)
+                }
+                deleteFromBD(sameNote)
+                renderList(noteList, ACTION_DEL_NOTE)
+            }
+            .setNegativeButton(R.string.no_clear_list, null)
+        val alertClearList = builder.create()
+        alertClearList.show()
+    }
+
+    private fun findNoteWithId(id: String): NoteEntity? {
+        for (note in noteList) {
+            if (note.id == id) {
+                return note
+            }
+        }
+        return null
+    }
+
+    private fun renderList(noteList: ArrayList<NoteEntity>) {
+        adapter!!.setData(noteList)
+        adapter!!.notifyDataSetChanged()
+    }
+
+    private fun renderList(notes: List<NoteEntity>, action: String) {
+        adapter!!.setData(notes)
+        when (action) {
+            ACTION_DEL_NOTE -> adapter!!.notifyItemRemoved(positionToList)
         }
     }
 
-    public void initListBD(ArrayList<NoteEntity> noteList) {
-        noteList.clear();
-        noteCollection.orderBy(NoteMapping.Fields.DATE,
-                Query.Direction.DESCENDING)
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            Map<String, Object> doc = document.getData();
-                            NoteEntity note = NoteMapping.toNote(doc);
-                            noteList.add(note);
-                            Log.d(TAG, document.getId() + " => " + document.getData());
-                            adapter.notifyDataSetChanged();
-                        }
-                    } else {
-                        Log.w(TAG, "Error getting documents.", task.getException());
+    fun initListBD(noteList: ArrayList<NoteEntity>) {
+        noteList.clear()
+        noteCollection!!.orderBy(
+            NoteMapping.Fields.DATE,
+            Query.Direction.DESCENDING
+        )
+            .get()
+            .addOnCompleteListener { task: Task<QuerySnapshot> ->
+                if (task.isSuccessful) {
+                    for (document in task.result!!) {
+                        val doc = document.data
+                        val note = NoteMapping.toNote(doc)
+                        noteList.add(note)
+                        Log.d(TAG, document.id + " => " + document.data)
+                        adapter!!.notifyDataSetChanged()
                     }
-                });
+                } else {
+                    Log.w(TAG, "Error getting documents.", task.exception)
+                }
+            }
     }
 
-    public void addNoteToBD(final NoteEntity note) {
-        noteCollection.add(NoteMapping.toDocument(note))
-                .addOnSuccessListener(documentReference ->
-                        Log.d("BD", "Сделана новая запись в БД: " + note.getTitle()));
-        adapter.notifyDataSetChanged();
+    fun addNoteToBD(note: NoteEntity) {
+        noteCollection!!.add(NoteMapping.toDocument(note))
+            .addOnSuccessListener { documentReference: DocumentReference? ->
+                Log.d(
+                    "BD",
+                    "Сделана новая запись в БД: " + note.title
+                )
+            }
+        adapter!!.notifyDataSetChanged()
     }
 
-    private void updateNoteFromBD(NoteEntity sameNote) {
+    private fun updateNoteFromBD(sameNote: NoteEntity) {
         noteCollection
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            Map<String, Object> doc = document.getData();
-                            if (doc.get("id").equals(sameNote.getId())) {
-                                String id = document.getId();
-                                noteCollection.document(id).set(NoteMapping.toDocument(sameNote));
-                            }
-                            Log.d(TAG, document.getId() + " => " + document.getData());
-                            adapter.notifyDataSetChanged();
+            ?.get()
+            ?.addOnCompleteListener { task: Task<QuerySnapshot> ->
+                if (task.isSuccessful) {
+                    for (document in task.result!!) {
+                        val doc = document.data
+                        if (doc["id"] == sameNote.id) {
+                            val id = document.id
+                            noteCollection!!.document(id).set(NoteMapping.toDocument(sameNote))
                         }
-                    } else {
-                        Log.w(TAG, "Error update documents.", task.getException());
+                        Log.d(TAG, document.id + " => " + document.data)
+                        adapter!!.notifyDataSetChanged()
                     }
-                });
+                } else {
+                    Log.w(TAG, "Error update documents.", task.exception)
+                }
+            }
     }
 
-    private void deleteFromBD(NoteEntity sameNote) {
+    private fun deleteFromBD(sameNote: NoteEntity?) {
         noteCollection
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            Map<String, Object> doc = document.getData();
-                            if (doc.get("id").equals(sameNote.getId())) {
-                                String id = document.getId();
-                                noteCollection.document(id).delete();
-                            }
-                            Log.d(TAG, document.getId() + " => " + document.getData());
-                            adapter.notifyDataSetChanged();
+            ?.get()
+            ?.addOnCompleteListener { task: Task<QuerySnapshot> ->
+                if (task.isSuccessful) {
+                    for (document in task.result!!) {
+                        val doc = document.data
+                        if (doc["id"] == sameNote!!.id) {
+                            val id = document.id
+                            noteCollection!!.document(id).delete()
                         }
-                    } else {
-                        Log.w(TAG, "Error remove documents.", task.getException());
+                        Log.d(TAG, document.id + " => " + document.data)
+                        adapter!!.notifyDataSetChanged()
                     }
-                });
+                } else {
+                    Log.w(TAG, "Error remove documents.", task.exception)
+                }
+            }
     }
 
-    private Controller getController() {
-        return ((Controller) getActivity());
+    private val controller: Controller?
+        private get() = activity as Controller?
+
+    internal interface Controller {
+        fun createNewNote()
+        fun deleteNote(delNote: NoteEntity?)
+        fun editNote(noteEntity: NoteEntity?)
     }
 
-    interface Controller {
-        void createNewNote();
-
-        void deleteNote(NoteEntity delNote);
-
-        void editNote(NoteEntity noteEntity);
+    companion object {
+        private const val TAG = "NoteSourceFirebaseImpl"
+        private const val positionToList = 0
     }
 }
